8141ad98601cf16927806a757a265dab8c50b50b
Koliur Rahman <kr5360@bhsec.bard.edu>
2024-01-26 22:48:32 -0500

    give pokemon options when typing

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,145 +1,139 @@
 import React from "react";
 import Link from "next/link";
 
-
 export default function Header() {
-    const includedPages = [
-        'Silhouette', 
-        'Zoom',
-        'Pokedex',
-        'Type',
-        'Item'
-    ]
-    const [width, setWidth] = React.useState<any>(700)
-    const [menuOpen, setMenuOpen] = React.useState(false)
+  const includedPages = ["Silhouette", "Zoom", "Pokedex", "Type", "Item"];
+  const [width, setWidth] = React.useState<any>(700);
+  const [menuOpen, setMenuOpen] = React.useState(false);
 
-    React.useEffect(() => {
-        const handleResize = () => {
-            setWidth(window.innerWidth)
-        }
-        handleResize()
-        window.addEventListener('resize', handleResize)
-        return (() => {
-            window.removeEventListener('resize', handleResize)
-        })
-    }, [])
+  React.useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-    const isMobile = width <= 768
-    const createNavItems = () => includedPages.map(label => (
-        <h2 
-            className="nav-item" 
-            key={label}
-            onClick={() => setMenuOpen(false)}
-        >
-            <style jsx>{`
-                .nav-item {
-                    color: ${isMobile? '#eeeeee': 'gray'};
-                    transition: .2s;
-                }
-                .nav-item:hover {
-                    color: #eeeeee;
-                    opacity: 1;
-                }    
-            `}</style>
-
-            <Link
-                className="link"
-                href={`/Quiz/${label}`}
-            >{label}</Link>
-        </h2>
-    ))
-
-    return (
-    <div className="Header">
+  const isMobile = width <= 768;
+  const createNavItems = () =>
+    includedPages.map((label) => (
+      <h2
+        className="nav-item text-2xl text-gray-400 hover:text-gray-300 py-5 font-bold"
+        key={label}
+        onClick={() => setMenuOpen(false)}
+      >
         <style jsx>{`
-            .Header {
-                width: 100%;
-                height: 100px;
-                z-index: 10;
-            }
-            .nav-container {
-                padding: 4px 30px;
-                padding-right: 0px;
-                /*background-color: rebeccapurple;*/
-                position: fixed;
-                left: 0;
-                right: 0;
-                top: 0;
-                display: flex;
-                flex-direction: row;
-                gap: 30px;
-                z-index: 10;
-            }
-
-            button {
-                border: none;  
-                background: none;              
-            }
-            .hamburger {
-                margin-left: auto;
-                padding: 20px;
-            }
-            .hamburger-button {
-            }
-
-            .modal {
-                position: fixed;
-                left: 0; top: 0; bottom: 0; right: 0;
-                display: flex; 
-                flex-direction: column;
-                background-color: black;
-                transition: .3s;
-            }
-            .opened {
-                transform: translateX(0%);
-            }
-            .closed {
-                transform: translateX(100%);
-            }
-
-            .modal > button {
-                align-self: flex-end;
-                padding: 30px 30px;
-                z-index: 5;
-            }
-            .mobile-menu {
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-                align-self: center;
-            }
+          .nav-item {
+            color: ${isMobile ? "#eeeeee" : "gray"};
+            transition: 0.2s;
+          }
+          .nav-item:hover {
+            color: #eeeeee;
+            opacity: 1;
+          }
         `}</style>
 
-        <div className="nav-container"> 
-            {isMobile 
-            ?
-                <div className="hamburger">
-                    <button className="hamburger-button" onClick={() => setMenuOpen(true)}>
-                        <img
-                            src='/menu-icon.svg'
-                            alt='menu icon'
-                            width='30px'
-                            height='30px'
-                        />
-                        {/* <h2>
+        <Link className="" href={`/Quiz/${label}`}>
+          {label}
+        </Link>
+      </h2>
+    ));
+
+  return (
+    <div className="Header">
+      <style jsx>{`
+        .Header {
+          width: 100%;
+          height: 100px;
+          z-index: 10;
+        }
+        .nav-container {
+          padding: 4px 30px;
+          padding-right: 0px;
+          /*background-color: rebeccapurple;*/
+          position: fixed;
+          left: 0;
+          right: 0;
+          top: 0;
+          display: flex;
+          flex-direction: row;
+          gap: 30px;
+          z-index: 10;
+        }
+
+        button {
+          border: none;
+          background: none;
+        }
+        .hamburger {
+          margin-left: auto;
+          padding: 20px;
+        }
+        .hamburger-button {
+        }
+
+        .modal {
+          position: fixed;
+          left: 0;
+          top: 0;
+          bottom: 0;
+          right: 0;
+          display: flex;
+          flex-direction: column;
+          background-color: black;
+          transition: 0.3s;
+        }
+        .opened {
+          transform: translateX(0%);
+        }
+        .closed {
+          transform: translateX(100%);
+        }
+
+        .modal > button {
+          align-self: flex-end;
+          padding: 30px 30px;
+          z-index: 5;
+        }
+        .mobile-menu {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          align-self: center;
+        }
+      `}</style>
+
+      <div className="nav-container">
+        {isMobile ? (
+          <div className="hamburger">
+            <button
+              className="hamburger-button"
+              onClick={() => setMenuOpen(true)}
+            >
+              <img
+                src="/menu-icon.svg"
+                alt="menu icon"
+                width="30px"
+                height="30px"
+              />
+              {/* <h2>
                             Menu
                         </h2> */}
-                    </button>
+            </button>
 
-                    <div className={`modal ${menuOpen? 'opened': 'closed'}`}>
-                        <button
-                            onClick={() => setMenuOpen(false)}
-                        >X</button>
-                        <div className="mobile-menu">
-                            {createNavItems()}
-                        </div>
-                    </div>
-                </div>
-            :
-                createNavItems()
-            }
-            {}
-        </div>    
+            <div className={`modal ${menuOpen ? "opened" : "closed"}`}>
+              <button onClick={() => setMenuOpen(false)}>X</button>
+              <div className="mobile-menu">{createNavItems()}</div>
+            </div>
+          </div>
+        ) : (
+          createNavItems()
+        )}
+        {}
+      </div>
     </div>
-    )
+  );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,114 +1,157 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import React from 'react';
-import Header from '../components/Header';
-import { useRouter } from 'next/router';
+import "../styles/globals.css";
+// import '@/styles/globals.css'
+// import type { AppProps } from "next/app";
+import React, { useEffect, useState } from "react";
+import Header from "../components/Header";
+import { useRouter } from "next/router";
 
-const Context = React.createContext({});
+export const PokeList = React.createContext({});
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const [loading, setLoading] = React.useState(false);
+  const [pokes, setPokes] = useState({});
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+      .then((res) => res.json())
+      .then((data) => {
+        const p = {};
+        data.results?.forEach((poke) => {
+          p[poke.name] = poke.url;
+        });
+        console.log({ p });
+        setPokes(p);
+      });
+  }, []);
 
   React.useEffect(() => {
     const handleStart = (url: string) => {
-      setLoading(true)
-      setTimeout(() => setLoading(false), 1000)
-    }
-    const handleComplete = (url: string) => {setLoading(false)}
-    router.events.on('routeChangeStart', handleStart)
-    router.events.on('routeChangeComplete', handleComplete)
+      setLoading(true);
+      setTimeout(() => setLoading(false), 1000);
+    };
+    const handleComplete = (url: string) => {
+      setLoading(false);
+    };
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
 
     return () => {
-      router.events.off('routeChangeStart', handleStart)
-      router.events.off('routeChangeComplete', handleComplete)
-    }
-  })
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+    };
+  }, []);
 
-  return <Context.Provider value={{yo: ''}}>
-    <div className='App'>
-      <Header/>
-      <Component {...pageProps} />
+  return (
+    <PokeList.Provider value={pokes}>
+      <div
+        className="App"
+        style={{ height: "100%", display: "flex", flexDirection: "column" }}
+      >
+        <Header />
+        <Component {...pageProps} />
 
-      <style jsx>{`
-        @keyframes rotating { /* ease-in-out */
-          0% {
-            transform: rotate(-30deg);
+        <style jsx>{`
+          @keyframes rotating {
+            /* ease-in-out */
+            0% {
+              transform: rotate(-30deg);
+            }
+            5% {
+              transform: rotate(-30deg);
+            }
+            50% {
+              transform: rotate(390deg);
+            }
+            55% {
+              transform: rotate(390deg);
+            }
+            100% {
+              transform: rotate(330deg);
+            }
           }
-          5% {
-            transform: rotate(-30deg);
+          @keyframes three-sixty {
+            from {
+              transform: rotate(0deg);
+            }
+            to {
+              transform: rotate(360deg);
+            }
           }
-          50% {
-            transform: rotate(390deg);
+          .loading-container {
+            position: fixed;
+            bottom: 12px;
+            right: 12px;
+            animation: three-sixty 0.5s linear infinite;
+            opacity: 0.9;
           }
-          55% {
-            transform: rotate(390deg);
+          .loading {
+            display: flex;
+            flex-direction: column;
+            transition: 0.3s;
+            transform: scale(1);
           }
-          100% {
-            transform: rotate(330deg);
+          .out {
+            transform: scale(0);
           }
-        }
-        @keyframes three-sixty {
-          from {
-            transform: rotate(0deg);
+          .ball {
+            width: 50px;
+            height: 25px;
+            border-radius: 25px;
+            border: solid 4px black;
           }
-          to {
-            transform: rotate(360deg);
+          .top {
+            background-color: red;
+            border-bottom-left-radius: 0;
+            border-bottom-right-radius: 0;
           }
-        }
-        .loading-container {
-          position: fixed;
-          bottom: 12px;
-          right: 12px;
-          animation: three-sixty .5s linear infinite;
-          opacity: .9;
-        }
-        .loading {
-          display: flex;
-          flex-direction: column;
-          transition: .3s;
-          transform: scale(1);
-        }
-        .out {
-          transform: scale(0);
-        }
-        .ball {
-          width: 50px;
-          height: 25px;
-          border-radius: 25px;
-          border: solid 4px black;
-        }
-        .top {
-          background-color: red;
-          border-bottom-left-radius: 0;
-          border-bottom-right-radius: 0;
-        }
-        .bottom {
-          background-color: white;
-          border-top-left-radius: 0;
-          border-top-right-radius: 0;
-        }
-        .dot {
-          position: absolute;
-          left: 50%;
-          top: 50%;
-          width: 15px;
-          height: 15px;
-          border: solid 4px black;
-          border-radius: 15px;
-          transform: translateX(-50%) translateY(-50%);
-          background-color: white;
-        }
-      `}</style>
+          .bottom {
+            background-color: white;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+          }
+          .dot {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            width: 15px;
+            height: 15px;
+            border: solid 4px black;
+            border-radius: 15px;
+            transform: translateX(-50%) translateY(-50%);
+            background-color: white;
+          }
+        `}</style>
 
-      <div className={`loading-container`}>
-        <div className={`loading ${loading? "in": "out"}`}>
-          <div className='ball top'/>
-          <div className='ball bottom'/>
-          <div className='dot'/>
+        <div className={`loading-container`}>
+          <div className={`loading ${loading ? "in" : "out"}`}>
+            <div className="ball top" />
+            <div className="ball bottom" />
+            <div className="dot" />
+          </div>
         </div>
       </div>
-    </div>
-  </Context.Provider>
-  
+    </PokeList.Provider>
+  );
+}
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+  );
+  const data = await res.json();
+  const pokes = {};
+  data.results?.forEach((poke) => {
+    pokes[poke.name] = poke.url;
+  });
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      pokes: data,
+    },
+  };
 }
